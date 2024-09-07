453bc17ca6d993427a90af185ff243cab0dbc37b
민정 <minjaung5073@naver.com>
2024-09-07 20:05:34 +0900

    h1의 classList에 classname이 포함되었으면 제거하고 없으면 추가

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const h1 = document.querySelector("div.hello:first-child h1")
 //첫번째 자식 h1의 스타일을 자바스크립트에서 변경하기
 
 function handleTitleClick() {// 클릭이벤트 시 색깔변화  함수
-    if (h1.className === "clicked") { //h1의 클래스명이 clicked(빨강)면 비우기=>파란색으로 만들기
-        h1.className = "";
+   const clickedClass="clicked"//raw value사용시 발생가능한 에러 방지
+
+    if (h1.classList.contains(clickedClass)) { //h1의 클래스명이 clicked(빨강)면 비우기=>파란색으로 만들기
+        h1.classList.remove(clickedClass);
     } else {
-        h1.className = "clicked";//빨간색이 아니면 빨강으로 바꾸기
+        h1.classList.add(clickedClass);
+        //h1의 classList에 classname이 포함되었으면 제거하고 없으면 추가
     }
 }
 
