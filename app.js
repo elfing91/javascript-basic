95dfb6d492c10820f0933dda78326403c432b996
민정 <minjaung5073@naver.com>
2024-09-07 19:29:36 +0900

    getter/setter변수를 이용해 클릭 이벤트시  색깔변화 함수 생성

--- app.js
+++ app.js
@@ -5,33 +5,25 @@ const title = document.querySelector(".hello:first-child h1")
 //첫번째 자식 h1의 스타일을 자바스크립트에서 변경하기
 
 function handleTitleClick() {// 클릭이벤트 시 색깔변화  함수
-    if (title.style.color == "black") {
-        title.style.color = "blue"
+    const currentColor = title.style.color//getter로써, 최근 color값을 복사하는 역할
+    let newColor;//setter로써, 변수에 대입된 색상값을 h1.style.color에 최종적으로 할당
+    if (currentColor == "blue") {
+        newColor = "tomato"
     } else {
-        title.style.color = "black";
+        newColor = "blue";
     }
+    title.style.color = newColor //현재 바뀐 색을 newColor로 설정
+
+    /*
+    1) click event 발생 및 함수 실행
+2) currentColor 변수 선언 후 h1.style.color 값 복사 (getter)
+3) newColor 변수 선언
+4) currentColor 현재 값 확인
+5) 조건에 따라 newColor에 "tomato" or "blue" 값 대입
+6) 마지막으로 h1.style.color에 newColor값 대입 (setter)
+    */
 }
 
-function handleMouseEnter() { //title메 마우스 올릴 때 이벤트 함수
-    title.innerText = "mouse is here"
-}
-function handleMouseLeave() {//title에 마우스 뗄때 이벤트 함수
-    title.innerText = "mouse is not here"
-}
-
-function handleWindowResize(){//윈도우의 사이즈변경 이벤트시 함수-배경색 변경
-    document.body.style.backgroundColor="tomato"
-}
-
-function handleWindowCopy(){//문자 복사 이벤트 시 함수--알림창 출력
-    alert("copied!")
-}
-
-title.onclick=handleTitleClick //아래와 같은 기능 수행
-//title.addEventListener("click", handleTitleClick)
-title.addEventListener("mouseenter", handleMouseEnter)
-title.addEventListener("mouseleave", handleMouseLeave)
 
 
-window.addEventListener("resize",handleWindowResize)
-window.addEventListener("copy",handleWindowCopy)
+title.addEventListener("click", handleTitleClick)
