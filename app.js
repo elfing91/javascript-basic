496a0fa959e7df85062566e7b20cd706344b40bf
민정 <minjaung5073@naver.com>
2024-09-08 23:21:09 +0900

    입력값의 조건에 따라 다르게 출력

--- app.js
+++ app.js
@@ -2,8 +2,17 @@
 const loginInput=document.querySelector("#login-form input")
 const loginButton=document.querySelector("#login-form button")
 
-function onLoginClick(){
-    console.log("click");
+function onLoginClick(){//버튼 클릭 이벤트 함수
+   
+    const value=loginInput.value;
+
+    if(value==""){
+        alert("write")
+    }else if(value.length>15){/// 글자수가 15이상이면 에러창 출력
+        alert("too long")     
+    }else{
+        console.log(value)
+    }
 }
 
 loginButton.addEventListener("click",onLoginClick)
