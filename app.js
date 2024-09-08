e1eb41823826551b6558571e84910a536112f135
민정 <minjaung5073@naver.com>
2024-09-09 00:36:47 +0900

    브라우저 기본 동작을 막는 함수 추가

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ const loginForm=document.querySelector("#login-form")
 const loginInput=document.querySelector("#login-form input")
 const loginButton=document.querySelector("#login-form button")
 
-function onLoginSubmit(){//로그인 제출시 이벤트 함수
-   
-    const value=loginInput.value;
-    console.log(value)
+function onLoginSubmit(tomato){//로그인 제출시 이벤트 함수
+    //form을 submit하면 브라우저는 기본으로 새로고침한다
+    tomato.preventDefault()//브라우저 기본 동작을 막는 함수
+    console.log(tomato)
 }
 
 loginForm.addEventListener("submit",onLoginSubmit)
 //form의 submit이벤트를 감지=>입력값 출력함
+
+
+//submit 이벤트가 발생시 JS는 onLoginSubmit함수를 호출하고 wvent 오브젝트를 argument로 전달
+//
