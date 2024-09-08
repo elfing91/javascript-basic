cb047d71707ec5a4ee195e034f9c69a3af66bab8
민정 <minjaung5073@naver.com>
2024-09-08 23:03:41 +0900

    html 엘리먼트를 연결하는 문구를 다른방식으로 표현/ 로그인 버튼 클릭 이벤트함수 추가

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
-const h1 = document.querySelector("div.hello:first-child h1")
-//class hello를 가진 div내부의 first자식인 h1찾기
-//=>HTML element를 자바스크립트로 가져오기
 
-//첫번째 자식 h1의 스타일을 자바스크립트에서 변경하기
+const loginInput=document.querySelector("#login-form input")
+const loginButton=document.querySelector("#login-form button")
 
-function handleTitleClick() {// 클릭이벤트 시 색깔변화  함수
-   const clickedClass="clicked"//raw value사용시 발생가능한 에러 방지
-    h1.classList.toggle(clickedClass) 
-    //토글은 h1의 classList에 clicked 클래스가 있는지 확인해 있으면 없애고 없으면 추가
-   
+function onLoginClick(){
+    console.log("click");
 }
 
-
-
-h1.addEventListener("click", handleTitleClick);
-
+loginButton.addEventListener("click",onLoginClick)
