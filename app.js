94ada0389b203345c95cf3951cbde32216412a22
민정 <minjaung5073@naver.com>
2024-09-09 13:13:56 +0900

    입력을 submit하면 form을 숨기도록 하기

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const loginForm=document.querySelector("#login-form")
+const loginForm = document.querySelector("#login-form")
 
-const loginInput=document.querySelector("#login-form input")
-const loginButton=document.querySelector("#login-form button")
+const loginInput = document.querySelector("#login-form input")
+const loginButton = document.querySelector("#login-form button")
+const greeting=document.getElementById("greeting")
 
-function onLoginSubmit(tomato){//로그인 제출시 이벤트 함수
-    //form을 submit하면 브라우저는 기본으로 새로고침한다
-    tomato.preventDefault()//브라우저 기본 동작을 막는 함수
-    console.log(tomato)
+function onLoginSubmit(tomato) {//로그인 제출시 이벤트 함수
+    const value = loginInput.value;
+    tomato.preventDefault()//브라우저 기본 동작을 막음
+    loginForm.classList.add("hidden")//hidden이라는 클래스명 더해 form을 숨김
+    greeting.innerText="hello,"+value //h1안에 greeting에 있는 텍스트 추가
 }
 
-loginForm.addEventListener("submit",onLoginSubmit)
+loginForm.addEventListener("submit", onLoginSubmit)
 //form의 submit이벤트를 감지=>입력값 출력함
 
 
