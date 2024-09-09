fd480c65276182607271ab0a809e017851614806
민정 <minjaung5073@naver.com>
2024-09-09 13:19:17 +0900

    엘리먼트를 불러와 클래스리스트중 특정 클래스를 추가하거나 삭제->h1의 숨김 클래스 제거

--- app.js
+++ app.js
@@ -4,11 +4,17 @@ const loginInput = document.querySelector("#login-form input")
 const loginButton = document.querySelector("#login-form button")
 const greeting=document.getElementById("greeting")
 
+//여러개의 string이 사용되므로 이를 저장하기 위한 변수 선언->대문자로 표기
+const HIDDEN_CLASSNAME="hidden"
+
 function onLoginSubmit(tomato) {//로그인 제출시 이벤트 함수
     const value = loginInput.value;
     tomato.preventDefault()//브라우저 기본 동작을 막음
-    loginForm.classList.add("hidden")//hidden이라는 클래스명 더해 form을 숨김
+    loginForm.classList.add(HIDDEN_CLASSNAME)//loginForm엘리먼트에 hidden이라는 클래스명 더해 form을 숨김
     greeting.innerText="hello,"+value //h1안에 greeting에 있는 텍스트 추가
+    
+    greeting.classList.remove(HIDDEN_CLASSNAME)//h1은 hidden을 해제해 숨겨지지 않도록 함
+    //id가 greeting인 h1엘리먼트를 불러와 클래스리스트 중 hidden이 있으면 제거함
 }
 
 loginForm.addEventListener("submit", onLoginSubmit)
